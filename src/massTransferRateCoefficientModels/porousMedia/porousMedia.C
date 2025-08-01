e9d7ec4445c2766704dbac1959f6a46611edd1ed
Filipe Brandao <flbrandao@kl4.head.cm.kestrel.hpc.nrel.gov>
2025-08-01 14:37:56 -0600

    bug fixed

--- src/massTransferRateCoefficientModels/porousMedia/porousMedia.C
+++ src/massTransferRateCoefficientModels/porousMedia/porousMedia.C
@@ -84,7 +84,16 @@ Foam::massTransferRateCoefficientModels::porousMedia::k()
     
     //- Loop over cells adjacent to transfer patch
     const labelList& transferCells = mesh_.boundary()[patchID_].faceCells();
+
+
+    // Create an array to hold the area of the faces
+    scalarField faceAreas(transferCells.size());
     
+    scalar dum1 = 0.0;
+    scalar dum2 = 0.0;
+    scalar dum3 = 0.0;
+    scalar dum4 = 0.0;
+    scalar dum5 = 0.0;
     forAll(transferCells, facei)
     {
         const label celli = transferCells[facei];
@@ -92,13 +101,31 @@ Foam::massTransferRateCoefficientModels::porousMedia::k()
         //- Calculate correlation quantities
         const scalar beta = Foam::pow(mu[facei] / (rho[facei] * D_), 0.333);
         const scalar gamma
-            = Foam::pow(rho[facei] * Ub[facei] * dp_
+            = Foam::pow(rho[facei] * mag(Ub[facei]) * dp_
                         / (mu[facei] * (1.0 - eps_)), 0.8);
         
         //- Set rate coefficient
         k_[celli] = C_ * beta * gamma * D_ / dp_;
+
+	// Get the face index for the current cell
+        const label faceIndex = mesh_.boundary()[patchID_].faceCells()[facei];
+
+        // Calculate face area using the mesh object
+        const scalar area = mag(mesh_.faceAreas()[faceIndex]);
+        faceAreas[facei] = area;
+
+	dum1 += mag(Ub[facei]) * area;
+	dum2 += k_[celli] * area;
+	dum3 += gamma * area;
+	dum4 += beta * area;
+	dum5 += area;
     }
     
+    Info << "Surf averaged mag(Ub): " << dum1/dum5 << endl;
+    Info << "Surf averaged gamma: " << dum3/dum5 << endl;
+    Info << "Surf averaged beta: " << dum4/dum5 << endl;
+    Info << "Surf averaged kg: " << dum2/dum5 << endl;
+
     return k_;
 }
 
